78f2da69c71daa084524ba93d46e562fbd61933b
Neo-VECTR <71106709+GareBear99@users.noreply.github.com>
2026-04-09 03:53:09 -0700

    Add files via upload

--- src/cognition_services/__init__.py
+++ src/cognition_services/__init__.py
@@ -6,3 +6,10 @@ from .cognition_core import CognitionCore, CognitionTier, TierChangeEvent
 from .cognition_core import TIER_PRIMARY, TIER_SECONDARY, TIER_LOCAL, TIER_ECHO, TIER_ORDER
 from .stream_session import StreamSession, StreamSessionManager, StreamReceipt, SessionMeta
 from .fallback_consciousness import FallbackConsciousnessLayer, FallbackEpisode
+from .critic import CriticService, CritiqueResult
+from .goal_synthesizer import GoalSynthesizer, SynthesisRule, SynthesizedGoal
+from .curriculum_trainer import CurriculumTrainer, TrainingDecision
+from .drift_detector import DriftDetector, DriftReport, DriftThresholds, ReferenceReceipt, SignalReading
+from .immutable_baseline import ImmutableBaseline, BaselineDecision, ProtectionTier, ProtectedPath
+from .reflection_service import ReflectionService, ReflectionReport
+from .episodic_retrieval import EpisodicRetrieval, EpisodicResult, EpisodicMatch
